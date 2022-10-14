bde9a3fadc495ecc4862212365762d9b10bf9df2
Andy-flower-github <86655544+Andy-flower-github@users.noreply.github.com>
2022-10-14 19:35:30 +0800

    Update SampleCode.cs

--- SampleCode.cs
+++ SampleCode.cs
@@ -1,6 +1,6 @@
-      public IEnumerable<Dto_ShipOrderList> GetShipOrderLists(SalesQueryParameter parameter)
-        {
-            var filterExpr = Expr.CreateMyExpression<Dto_ShipOrderList>()
+public IEnumerable<Dto_ShipOrderList> GetShipOrderLists(SalesQueryParameter parameter)
+{
+      var filterExpr = Expr.CreateMyExpression<Dto_ShipOrderList>()
                 .And(Expr.Equal, nameof(Dto_ShipOrderList.Status), "C")
                 .MaybeAnd(Expr.GreaterThanOrEqual, nameof(Dto_ShipOrderList.ShipDate), parameter.StartDate)
                 .MaybeAnd(Expr.LessThanOrEqual, nameof(Dto_ShipOrderList.ShipDate), parameter.EndDate)
@@ -15,9 +15,22 @@
                 .MaybeAnd(parameter.ShipTypeOption == ShipTypeOptions.Exclude_ShipToStore, Expr.NotEqual, nameof(Dto_ShipOrderList.ShipType), "3")
                 .MaybeAnd(parameter.ShipTypeOption == ShipTypeOptions.Exclude_Replacement, Expr.NotEqual, nameof(Dto_ShipOrderList.ShipType), "4");
 
-            return MyUsing(ContextHelper.GetContext(),
-                            context => context.Set<Dto_ShipOrderList>().AsNoTracking()
-                            .Where(filterExpr.GetLambda())
-                            .OrderBy(x => x.ShipID)
-                            .ToList());
-        }
+      return MyUsing(ContextHelper.GetContext(),
+                     context => context.Set<Dto_ShipOrderList>()
+                                  .Where(filterExpr.GetLambda())
+                                  .OrderBy(x => x.ShipID)
+                                  .ToList());
+}
+
+public void Test()
+{
+      var myExpr = Expr.CreateMyExpression<Customer>("x");
+      var filter = myExpr.And(Expr.GreaterThan, nameof(Customer.CustomerID), "C0000001")
+                         .And(Expr.Like, nameof(Customer.CustomerID), "%700%")
+                         .And(Expr.StartsWith, nameof(Customer.CustomerID), "H")
+                         .And(Expr.GreaterThan, nameof(Customer.PayDay), (byte)20)
+                         .MaybeAnd(myExpr.MaybeOr(Expr.Contains, nameof(Customer.CustomerName), "醫院")
+                                         .MaybeOr(Expr.Contains, nameof(Customer.CustomerName), "診所")
+                                         .BodyExpr)
+                         .GetLambda();
+}
